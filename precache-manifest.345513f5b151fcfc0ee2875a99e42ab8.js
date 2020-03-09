3c5bacc2cb9745e8c90d2bc97b0f41791cce558b
Eldar Abdullazyanov <eldar.abdullazyanov@akvelon.com>
2020-03-10 01:21:07 +0300

    Updates

---
+++ precache-manifest.345513f5b151fcfc0ee2875a99e42ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fd845cb190617db1ff721ec6ab5be7",
+    "url": "eabdullazyanov.github.io/tic-tac-toe-with-ai/index.html"
+  },
+  {
+    "revision": "8d593a75d74134cdba5b",
+    "url": "eabdullazyanov.github.io/tic-tac-toe-with-ai/static/css/main.fd3e3e16.chunk.css"
+  },
+  {
+    "revision": "68f0fe6a53ec5a4a28a8",
+    "url": "eabdullazyanov.github.io/tic-tac-toe-with-ai/static/js/2.ba0e149b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "eabdullazyanov.github.io/tic-tac-toe-with-ai/static/js/2.ba0e149b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d593a75d74134cdba5b",
+    "url": "eabdullazyanov.github.io/tic-tac-toe-with-ai/static/js/main.a8f12e7e.chunk.js"
+  },
+  {
+    "revision": "6e28f7c21f337908d076",
+    "url": "eabdullazyanov.github.io/tic-tac-toe-with-ai/static/js/runtime-main.796b8459.js"
+  }
+]);
